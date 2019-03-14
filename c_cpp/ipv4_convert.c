bb54b0fade27160d6e4e4d3e9abc26c3ec197e76
qlliu <q_l_l@sina.com>
2019-03-14 10:34:46 +0800

    replace inet_addr with inet_pton to provide a cleaner way to indicate error return

--- c_cpp/ipv4_convert.c
+++ c_cpp/ipv4_convert.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <errno.h>
 
 void ipv4_str_2_net(char *ipv4)
 {
-	unsigned int netIp = inet_addr(ipv4);
-	printf("net ip=0x%X\n", netIp);
+	int ret;
+	unsigned int netIp;
+
+	ret = inet_pton(AF_INET, ipv4, &netIp);
+	if (1 == ret) {
+		printf("%s to network byte order is: 0x%X\n", ipv4, netIp);
+	}
+	else if (0 == ret) {
+		printf("%s incorrect\n", ipv4);
+	}
+	else {
+		printf("error occur: %d,%s\n", errno, strerror(errno));
+	}
+
+	return;
 }
 
-void ipv4_net_2_str(unsigned int netIpv4)
+void ipv4_net_2_str(unsigned int netIp)
 {
-	struct in_addr stAddr;
-	char strIpv4[32];
+	struct in_addr inAddr;
 
-	stAddr.s_addr = (in_addr_t)netIpv4;
-	snprintf(strIpv4, sizeof(strIpv4), "%s", inet_ntoa(stAddr));
+	inAddr.s_addr = netIp;
+	printf("0x%X to character string is: %s\n", netIp, inet_ntoa(inAddr));
 
-	printf("str ip=%s\n", strIpv4);
+	return;
 }
 
 int main(int argc, char *argv[])
 {
-	char strIpv4[] = "192.168.100.200";
-	unsigned int netIpv4 = 0xC964A8C0;
+	// character string to network byte order
+	ipv4_str_2_net((char *)"192.168.100.200");
 
-	ipv4_str_2_net(strIpv4);
-
-	ipv4_net_2_str(netIpv4);
+	// network byte order to character string
+	ipv4_net_2_str(0xC964A8C0);
 
 	return 0;
 }

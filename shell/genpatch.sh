edff12b2829b0bf296e280dc49e1d5ae5fd0acee
qlliu <q_l_l@sina.com>
2019-06-19 05:16:45 -0400

    genpatch.sh supports to diff two files

--- shell/genpatch.sh
+++ shell/genpatch.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-usage()
+print_usage()
 {
     printf "usage: $0 [OPTION] [file|two dirs]
 Options:
@@ -22,7 +22,7 @@ Examples:
 }
 
 ARGS=`getopt -a -o p:h -l strip-num:,help -- "$@"`
-[ $? -ne 0 ] && usage
+[ $? -ne 0 ] && print_usage
 eval set -- "${ARGS}"
 while true; do
     case "$1" in
@@ -31,7 +31,7 @@ while true; do
         shift 2
         ;;
     -h|--help)
-        usage
+        print_usage
         exit 0
         ;;
     --)
@@ -50,31 +50,34 @@ strip_num=${strip_num:-0}
 # rest arguments
 if [ $# == 1 ]; then
     if [ ! -f $1 ]; then # must be a file
-        echo "file usage"
+        print_usage
         exit 1
     fi
     if [ "${1:0-6}" != ".patch" ]; then # must be .patch as suffix
-        echo "unequal"
+        print_usage
         exit 1
     fi
 
     cat $1 | filterdiff --remove-timestamps --strip=${strip_num}
 elif [ $# == 2 ]; then
-    if [ ! -d $1 -o ! -d $2 ]; then # must be dir
-        echo "must be dir"
+    if [ -f $1 -a -d $2 ]; then # diff a file to a dir is forbidden
+        print_usage
+        exit 1
+    elif [ -d $1 -a -f $2 ]; then # diff a dir to a file is forbidden
+        print_usage
         exit 1
     fi
 
-    old_dir=$1
-    new_dir=$2
+    old_src=$1
+    new_src=$2
     if [ 0 == ${strip_num} ]; then
-        diff -urNa ${old_dir} ${new_dir} | filterdiff --remove-timestamps
+        diff -urNa ${old_src} ${new_src} | filterdiff --remove-timestamps
     else
-        diff -urNa ${old_dir} ${new_dir} | \
+        diff -urNa ${old_src} ${new_src} | \
             filterdiff --remove-timestamps --addoldprefix=a/ --addnewprefix=b/ --strip=${strip_num}
     fi
 else
-    echo "rest usage"
+    print_usage
     exit 1
 fi
 

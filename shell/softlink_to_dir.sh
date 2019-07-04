8a6aa863312de1346cfebe1bde88e8a39b2028ff
qlliu <q_l_l@sina.com>
2019-07-04 02:58:21 -0400

    softlink_to_dir script added make all shell scripts executable

---
+++ shell/softlink_to_dir.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+print_usage() {
+    printf "Usage:
+    $0 realfile_dir softlink_dir   - supports directory only
+"
+}
+
+if [ $# != 2 ]; then
+    print_usage
+    exit 1
+fi
+
+realfile_dir=$1
+softlink_dir=$2
+
+if [ ! -d ${realfile_dir} -o ! -d ${softlink_dir} ]; then
+    print_usage
+    exit 1
+fi
+
+list=`find ${realfile_dir} -not -path "*/.git*" -not -path "*/.svn*" | sed -e "s|^${realfile_dir}|\/.\/|g"`
+for v in ${list}; do
+    if [ -d "${realfile_dir}${v}" ]; then
+        mkdir -p "${softlink_dir}${v}"
+    elif [ -f "${realfile_dir}${v}" ]; then
+        ln -sf "${realfile_dir}${v}" "${softlink_dir}${v}"
+    else
+        echo "unknown file type: ${realfile_dir}${v}"
+    fi
+done
+
+exit 0
+

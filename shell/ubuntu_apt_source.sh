b90f4c11a8a944db68e3fef8fb87720a8c81e2b5
qlliu <q_l_l@sina.com>
2020-11-16 15:20:37 +0800

    shell: installing openssh-server & typo

--- shell/ubuntu_apt_source.sh
+++ shell/ubuntu_apt_source.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 codename=$(lsb_release -c | awk -F ' ' '{print $2}')
+sourceurl="http://mirrors.aliyun.com/ubuntu/"
 
 AptSourceUpdate()
 {
 	sudo bash -c "cat << EOF > /etc/apt/sources.list
-deb     http://mirrors.aliyun.com/ubuntu/ ${codename}           main restricted universe multiverse
-deb     http://mirrors.aliyun.com/ubuntu/ ${codename}-security  main restricted universe multiverse
-deb     http://mirrors.aliyun.com/ubuntu/ ${codename}-updates   main restricted universe multiverse
-deb     http://mirrors.aliyun.com/ubuntu/ ${codename}-proposed  main restricted universe multiverse
-deb     http://mirrors.aliyun.com/ubuntu/ ${codename}-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${codename}           main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${codename}-security  main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${codename}-updates   main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${codename}-proposed  main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${codename}-backports main restricted universe multiverse
+deb     ${sourceurl} ${codename}           main restricted universe multiverse
+deb     ${sourceurl} ${codename}-security  main restricted universe multiverse
+deb     ${sourceurl} ${codename}-updates   main restricted universe multiverse
+deb     ${sourceurl} ${codename}-proposed  main restricted universe multiverse
+deb     ${sourceurl} ${codename}-backports main restricted universe multiverse
+deb-src ${sourceurl} ${codename}           main restricted universe multiverse
+deb-src ${sourceurl} ${codename}-security  main restricted universe multiverse
+deb-src ${sourceurl} ${codename}-updates   main restricted universe multiverse
+deb-src ${sourceurl} ${codename}-proposed  main restricted universe multiverse
+deb-src ${sourceurl} ${codename}-backports main restricted universe multiverse
 EOF"
 }
 
 AptInstallTools()
 {
 	sudo apt update \
-	&& sudo apt -y install gcc g++ perl autoconf libssl-dev libncurses5-dev net-tools
+	&& sudo apt -y install gcc g++ perl autoconf libssl-dev libncurses5-dev net-tools openssh-server
 }
 
 AptSourceUpdate

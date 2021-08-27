71f29c2d0c8075b265017b7750cad428ba979626
qlliu <q_l_l@sina.com>
2021-08-27 14:27:52 +0800

    random_color.sh add parameter control

--- shell/random_color.sh
+++ shell/random_color.sh
@@ -70,15 +70,79 @@ rscolor() { # reset color
     echo -e "\033[0m"
 }
 
-for i in {1..10}; do
-    read -r text_bg text_fg < <(generate_color_pair)
+print_usage() {
+    printf "usage: %s [OPTION] [VALUE]
+Options:
+    --rgb               text displayed in rgb
+    --hex               text displayed in hex(default in hex)
+    --count N           display N times colors(N >= 0, default 8)
+    -h|--help           this help
 
-    #printf "$(fgcolor "${text_bg}")$(defbgcolor)$(reverse)$(rscolor)$(bgcolor "${text_bg}")$(fgcolor "${text_fg}") fg_rgb: ${text_fg} bg_rgb: ${text_bg} $(fgcolor "${text_bg}")$(defbgcolor)\n\n"
+Examples:
+    1: %s --rgb
+    2: %s --hex
+    3: %s --rgb --count 3
+" "$0" "$0" "$0" "$0"
 
-    IFS=';' read -ra bg_rgb_arr <<< "$text_bg"
-    IFS=';' read -ra fg_rgb_arr <<< "$text_fg"
-    printf "$(fgcolor "${text_bg}")$(defbgcolor)$(reverse)$(rscolor)$(bgcolor "${text_bg}")$(fgcolor "${text_fg}") fg_hex: #%02x%02x%02x bg_hex: #%02x%02x%02x $(fgcolor "${text_bg}")$(defbgcolor)\n\n" \
-        "${fg_rgb_arr[0]}" "${fg_rgb_arr[1]}" "${fg_rgb_arr[2]}" \
-        "${bg_rgb_arr[0]}" "${bg_rgb_arr[1]}" "${bg_rgb_arr[2]}"
+}
+
+main() {
+    local target="$1"
+    local count="$2"
+
+    for i in $(seq "$count"); do
+        read -r text_bg text_fg < <(generate_color_pair)
+
+        if [ "$target" == "rgb" ]; then
+            echo ""
+            echo "$i: $(fgcolor "${text_bg}")$(defbgcolor)$(reverse)$(rscolor)$(bgcolor "${text_bg}")$(fgcolor "${text_fg}") fg_rgb: ${text_fg} bg_rgb: ${text_bg} $(fgcolor "${text_bg}")$(defbgcolor)$(rscolor)"
+        else
+            IFS=';' read -ra bg_rgb_arr <<< "$text_bg"
+            IFS=';' read -ra fg_rgb_arr <<< "$text_fg"
+            echo ""
+            printf "$i: $(fgcolor "${text_bg}")$(defbgcolor)$(reverse)$(rscolor)$(bgcolor "${text_bg}")$(fgcolor "${text_fg}") fg_hex: #%02x%02x%02x bg_hex: #%02x%02x%02x $(fgcolor "${text_bg}")$(defbgcolor)$(rscolor)\n" \
+                "${fg_rgb_arr[0]}" "${fg_rgb_arr[1]}" "${fg_rgb_arr[2]}" \
+                "${bg_rgb_arr[0]}" "${bg_rgb_arr[1]}" "${bg_rgb_arr[2]}"
+        fi
+    done
+    echo ""
+}
+
+g_target="hex"
+g_count="8"
+
+if ! ARGS=$(getopt -a -o lh -l list,rgb,hex,count:,help -- "$@"); then
+    print_usage
+fi
+eval set -- "${ARGS}"
+while true; do
+    case "$1" in
+    --hex)
+        g_target="hex"
+        shift
+        ;;
+    --rgb)
+        g_target="rgb"
+        shift
+        ;;
+    --count)
+        g_count="$2"
+        shift 2
+        ;;
+    -h|--help)
+        print_usage
+        exit 0
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *)
+        echo "Internal error"
+        exit 1
+        ;;
+    esac
 done
+
+main "$g_target" "$g_count"
 

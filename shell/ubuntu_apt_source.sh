87b194a6a00366daaabf9ed93a46877c2d94e0a2
qlliu <q_l_l@sina.com>
2021-09-27 11:17:02 +0800

    remove installing depends

--- shell/ubuntu_apt_source.sh
+++ shell/ubuntu_apt_source.sh
@@ -17,24 +17,9 @@ deb-src ${sourceurl} ${codename}-updates   main restricted universe multiverse
 deb-src ${sourceurl} ${codename}-proposed  main restricted universe multiverse
 deb-src ${sourceurl} ${codename}-backports main restricted universe multiverse
 EOF"
-}
 
-AptInstallTools()
-{
     sudo apt update
-
-    local packages="gcc g++ perl autoconf libssl-dev libncurses5-dev net-tools openssh-server make"
-    IFS=' ' read -r -a arr <<< "$packages"
-    for p in "${arr[@]}"; do
-        if dpkg -s "$p" >/dev/null 2>&1; then
-            continue # package already installed
-        fi
-
-        sudo apt -y install "$p"
-    done
 }
 
 AptSourceUpdate
-
-AptInstallTools
 
